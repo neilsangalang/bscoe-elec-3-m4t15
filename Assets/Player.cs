2834315af9839740100ec54477c06d4e6d8e6872
Neil Adriane Sangalang <ite.nasangalang@gmail.com>
2018-02-19 19:30:31 +0800

    Added Player Controls; Refined ship movement (smoother rotation and maneuver)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,20 +1,136 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		print(xThrow);
-		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-		print(yThrow);
-	}
+
+public class Player : MonoBehaviour
+{
+
+    Vector3 currentPosition, currentRotation;
+    [SerializeField] float controlSpeed;
+    [SerializeField] float tiltSpeed;
+    [SerializeField] float xMinRange;
+    [SerializeField] float xMaxRange;
+    [SerializeField] float yMinRange;
+    [SerializeField] float yMaxRange;
+    [SerializeField] float yRotation;
+    [SerializeField] float xRotation;
+    float xThrow, yThrow;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        controlSpeed = 2f;
+        tiltSpeed = 10.0f;
+        xMinRange = -2.43f;
+        xMaxRange = 2.43f;
+        yMinRange = -2.43f;
+        yMaxRange = 2.43f;
+        yRotation = 0.0F;
+        xRotation = 0.0F;
+}
+
+    void RotateY()
+    {
+        if (yThrow == 0 && (transform.localEulerAngles.x >= 1 || transform.localEulerAngles.x <= -1))
+        { 
+
+            if (yRotation >= -15.0f && yRotation <= 0.0f)
+            {
+                if(yRotation >= -2.0f && yRotation <= 0.0f)
+                {
+                    yRotation += 1f;
+                }
+                else
+                {
+                    yRotation += 2f;
+                }
+            }
+            else if (yRotation <= 15.0f)
+            {
+                if (yRotation <= 2.0f)
+                {
+                    yRotation -= 1f;
+                }
+                else
+                {
+                    yRotation -= 2f;
+                }
+            }
+        }
+        else if (yThrow == 0)
+        {
+            yRotation = 0f;
+        }
+        yRotation += -yThrow * tiltSpeed * Time.deltaTime * 10;
+        yRotation = Mathf.Clamp(yRotation, -15.0f, 15.0f);
+    }
+
+    private void RotateX()
+    {
+        if (xThrow == 0 && (transform.localEulerAngles.z >= 1 || transform.localEulerAngles.z <= -1))
+        {
+
+            if (xRotation >= -20.0f && xRotation <= 0.0f)
+            {
+                if (xRotation >= -2.0f && xRotation <= 0.0f)
+                {
+                    xRotation += 1f;
+                }
+                else
+                {
+                    xRotation += 2f;
+                }
+            }
+            else if (xRotation <= 20.0f)
+            {
+                if (xRotation <= 2.0f)
+                {
+                    xRotation -= 1f;
+                }
+                else
+                {
+                    xRotation -= 2f;
+                }
+            }
+        }
+        else if (xThrow == 0)
+        {
+            xRotation = 0f;
+        }
+        xRotation += -xThrow * tiltSpeed * Time.deltaTime * 10;
+        xRotation = Mathf.Clamp(xRotation, -18.0f, 20.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentPosition = transform.localPosition;
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        print(xThrow);
+        print(yThrow);
+
+        currentPosition.x += xThrow * controlSpeed * Time.deltaTime;
+        currentPosition.y += yThrow * controlSpeed * Time.deltaTime;
+        currentPosition = new Vector3( Mathf.Clamp(currentPosition.x, xMinRange, xMaxRange), Mathf.Clamp(currentPosition.y, yMinRange, yMaxRange), transform.localPosition.z);
+        yRotation = Mathf.Clamp(yRotation, -15.0f, 15.0f);
+        xRotation = Mathf.Clamp(xRotation, -18.0f, 20.0f);
+
+        RotateY();
+        RotateX();
+
+        transform.localEulerAngles = new Vector3(Mathf.Clamp(yRotation, -15.0f, 30.0f), 0, Mathf.Clamp(xRotation, -20.0f, 20.0f));
+
+        if (transform.localEulerAngles.z >= 4 || transform.localEulerAngles.z <= -4 || transform.localEulerAngles.x >= 4 || transform.localEulerAngles.x <= -4 )
+            transform.localPosition = currentPosition;
+
+    }
+    
 }
