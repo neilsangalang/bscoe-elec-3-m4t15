24215e4430e0faa641f599a6c837080cb0de6aab
Neil Adriane Sangalang <ite.nasangalang@gmail.com>
2018-02-19 15:02:56 +0800

    Added M4T15 Assets Folder

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadSceneManager", 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		print(xThrow);
+		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		print(yThrow);
+	}
+}
