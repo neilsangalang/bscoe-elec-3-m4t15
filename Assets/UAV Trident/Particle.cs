a685e65f5b6f3590293a8ea559a7404c3f1684f9
Neil Adriane Sangalang <ite.nasangalang@gmail.com>
2018-03-09 08:33:30 +0800

    Added Enemies that explode on collision

---
+++ Assets/UAV Trident/Particle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour {
+    public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other) {
+        GameObject newExplosion = Instantiate (explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+
+
+
+
+}
