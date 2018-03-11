39946ef332b9a9ad9e1074bf6490bb0e9a74d3f6
Neil Adriane Sangalang <ite.nasangalang@gmail.com>
2018-03-11 22:19:26 +0800

    Added Particle System Auto Destroy script on Explosions

---
+++ Assets/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemAutoDestroy : MonoBehaviour {
+
+    private ParticleSystem particlesystem;
+
+    public void Start()
+    {
+        particlesystem = GetComponent<ParticleSystem>();
+    }
+
+    public void Update()
+    {
+        if (particlesystem)
+        {
+            if (!particlesystem.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
